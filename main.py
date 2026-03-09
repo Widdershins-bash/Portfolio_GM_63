8de0924e9c2d77b9f64f2e3893998e051471b3b7
Kaden Biondich <kbiondich7@gmail.com>
2026-03-09 14:38:08 -0500

    tool: Create test image

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pygame
 from game.tile import Tile
 from system.screen import Screen
-from system.constants import GRID_CONSTANT, GameState as gs
+from system.constants import GRID_CONSTANT, GameState as gs, IMAGE_PATH
 
 
 pygame.init()
@@ -22,6 +22,9 @@ if __name__ == "__main__":
     color_seq: float = 0
     game_state: gs = gs.PLAY
 
+    image_x: float = 300
+    image_y: float = 200
+
     while screen.running:
         for event in pygame.event.get():
             if screen.running:
@@ -36,6 +39,28 @@ if __name__ == "__main__":
         screen.logical.fill((red, green, blue))
 
         tile.create_platform(width=10, height=10)
+
+        # ----------------------------- testing image ------------------------------
+
+        placeholder_image: pygame.Surface = pygame.image.load(IMAGE_PATH + "Boat.png")
+
+        speed: float = 60 * delta_time
+
+        if pygame.key.get_pressed()[pygame.K_RIGHT]:
+            image_x += speed
+
+        if pygame.key.get_pressed()[pygame.K_LEFT]:
+            image_x -= speed
+
+        if pygame.key.get_pressed()[pygame.K_UP]:
+            image_y -= speed
+
+        if pygame.key.get_pressed()[pygame.K_DOWN]:
+            image_y += speed
+
+        screen.logical.blit(placeholder_image, (image_x, image_y))
+
+        # -------------------------------- end test --------------------------------
 
         screen.draw_overlay()
         screen.scale_flip()
