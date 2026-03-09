8f1dcb9ab4d37394d9bb6ceb10f1786bb5286f16
Kaden Biondich <kbiondich7@gmail.com>
2026-03-09 08:29:56 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,40 @@
+import pygame
+
+
+pygame.init()
+
+
+def get_delta_time(clock: pygame.Clock, fps: int):
+    delta_time: float = clock.tick(fps) / 1000
+    delta_time = max(0.001, min(0.1, delta_time))
+    return delta_time
+
+
+screen: pygame.Surface = pygame.display.set_mode((600, 600))
+
+clock: pygame.Clock = pygame.Clock()
+FPS: int = 60
+
+color_seq: float = 0
+running: bool = True
+while running:
+
+    delta_time: float = get_delta_time(clock=clock, fps=FPS)
+
+    red: int = int(color_seq) % 255
+    green: int = (int(color_seq) + 85) % 255
+    blue: int = (int(color_seq) + 170) % 255
+    screen.fill((red, green, blue))
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                running = False
+
+    color_seq += 50 * delta_time
+    pygame.display.flip()
+
+pygame.quit()
